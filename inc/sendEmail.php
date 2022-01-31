39c65aa593460b0309d39750b1782ec80acf1520
LeanCabeza <cabezaleandro@gmail.com>
2022-01-31 12:57:33 -0300

    Update sendEmail.php

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -1,4 +1,4 @@
-﻿<?php
+backup ﻿<?php
 
 // Replace this with your own email address
 $siteOwnersEmail = 'cabezaleandro@gmail.com';
@@ -42,16 +42,27 @@ if($_POST) {
 	$headers .= "Reply-To: ". $email . "\r\n";
  	$headers .= "MIME-Version: 1.0\r\n";
 	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
-	
- 	$mail = mail($siteOwnersEmail, $subject, $message, $headers);
 
-	if ($mail) { 
-		echo "OK"; 
-	} else {
-		echo "Something went wrong. Please try again."; 
-	}
+
+   if (!$error) {
+
+      ini_set("sendmail_from", $siteOwnersEmail); // for windows server
+      $mail = mail($siteOwnersEmail, $subject, $message, $headers);
+
+		if ($mail) { echo "OK"; }
+      else { echo "Something went wrong. Please try again."; }
 		
+	} # end if - no validation error
+
+	else {
+
+		$response = (isset($error['name'])) ? $error['name'] . "<br /> \n" : null;
+		$response .= (isset($error['email'])) ? $error['email'] . "<br /> \n" : null;
+		$response .= (isset($error['message'])) ? $error['message'] . "<br />" : null;
 		
+		echo $response;
+
+	} # end if - there was a validation error
 
 }
 
